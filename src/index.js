e29dfa8b462b2b9bbcd51071195837d0e95ec619
Amanda Hogan <ahogan@teamworks.com>
2020-02-17 20:17:15 -0500

    Set up store, provider, and reducer function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers/Reducer';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={createStore(reducer)}>
+    <App />
+  </Provider>, rootElement);
